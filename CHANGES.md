c264d71716f3f8936c6a39f8702d1efbab9a723f
Mike Hemberger <mike@thestizmedia.com>
2021-04-21 09:42:16 -0400

    Update the updater

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 0.3.1 (4/21/21)
+* Changed: Update updater.
+
 ## 0.3.0 (2/13/21)
 * Added: Mai logo icon to updater.
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e12f5c7f0887f2cf961dc15286c6debf25800c7f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e12f5c7f0887f2cf961dc15286c6debf25800c7f',
+    ),
+    'yahnis-elsts/plugin-update-checker' => 
+    array (
+      'pretty_version' => 'v4.11',
+      'version' => '4.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3155f2d3f1ca5e7ed3f25b256f020e370515af43',
+    ),
+  ),
+);
